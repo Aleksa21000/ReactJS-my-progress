7e0fd6d28226da96db10b40ba4f08a3a81316c08
Aleksa21000 <radulovicaleksa21@gmail.com>
2023-03-23 08:16:28 +0100

    better way of using context & introduced forwardRef with useImperativeHandle hook for forwarding ref to custom Input component

--- 04_00 project/src/store/auth-context.js
+++ 04_00 project/src/store/auth-context.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const AuthContext = React.createContext({});
+const AuthContext = React.createContext({
+	isLoggedIn: false,
+	onLogout: () => {},
+	onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	useEffect(() => {
+		const storedLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+		if (storedLoggedInInformation === "1") {
+			setIsLoggedIn(true);
+		}
+	}, []);
+
+	const loginHandler = (email, password) => {
+		localStorage.setItem("isLoggedIn", "1");
+		setIsLoggedIn(true);
+	};
+
+	const logoutHandler = () => {
+		localStorage.removeItem("isLoggedIn");
+		setIsLoggedIn(false);
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: isLoggedIn,
+				onLogout: logoutHandler,
+				onLogin: loginHandler,
+			}}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
+};
 
 export default AuthContext;
