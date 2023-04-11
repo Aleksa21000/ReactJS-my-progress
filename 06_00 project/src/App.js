4988265cd0789708d0a8324bc95ade0aa9f4f384
Aleksa21000 <radulovicaleksa21@gmail.com>
2023-04-11 16:35:02 +0200

    optimisation with useMemo

--- 06_00 project/src/App.js
+++ 06_00 project/src/App.js
@@ -5,16 +5,24 @@ import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+	const [allowToggle, setAllowToggle] = useState(false);
 	const [showParagraph, setShowParagraph] = useState(false);
 
+	const allowToggleHandler = () => {
+		setAllowToggle(true);
+	};
+
 	const showParagraphHandler = useCallback(() => {
-		setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-	}, []);
+		if (allowToggle) {
+			setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+		}
+	}, [allowToggle]);
 
 	return (
 		<div className="app">
 			<h1>Hi there!</h1>
 			<DemoOutput show={showParagraph} />
+			<Button onClick={allowToggleHandler}>Allow toggling</Button>
 			<Button onClick={showParagraphHandler}>Show Paragraph</Button>
 		</div>
 	);
