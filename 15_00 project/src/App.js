c164ac95ee6222998b1fd2a6e4072b0aab6f42c2
Aleksa21000 <radulovicaleksa21@gmail.com>
2024-02-28 20:50:07 +0100

    adding Layout component & rendering outlet components with children property

--- 15_00 project/src/App.js
+++ 15_00 project/src/App.js
@@ -1,7 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './pages/Root';
 import HomePage from './pages/Home';
+import Products from './pages/Products';
 
-const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{ path: '/', element: <HomePage /> },
+			{ path: '/products', element: <Products /> },
+		],
+	},
+]);
 
 function App() {
 	return <RouterProvider router={router} />;
