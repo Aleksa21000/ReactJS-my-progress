5eb8607aae9dfbb375b26b5a790702563a1404ed
Aleksa21000 <radulovicaleksa21@gmail.com>
2024-03-02 20:36:43 +0100

    added newsletter on two places & introduces useFetcher.Form that prevents redirect to orginal action place insted of Form component

--- 16_00 project/frontend/src/App.js
+++ 16_00 project/frontend/src/App.js
@@ -1,22 +1,23 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import RootPage from './pages/Root';
-import ErrorPage from './pages/Error';
-import Homepage from './pages/Home';
-import RootEventsPage from './pages/RootEvents';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import RootEventsPage from './pages/RootEvents';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <RootPage />,
+		element: <RootLayout />,
 		errorElement: <ErrorPage />,
 		children: [
-			{ index: true, element: <Homepage /> },
+			{ index: true, element: <HomePage /> },
 			{
 				path: 'events',
 				element: <RootEventsPage />,
@@ -31,12 +32,29 @@ const router = createBrowserRouter([
 						id: 'event-detail',
 						loader: eventDetailLoader,
 						children: [
-							{ index: true, element: <EventDetailPage />, action: deleteEventAction },
-							{ path: 'edit', element: <EditEventPage />, action: manipulateEventAction },
+							{
+								index: true,
+								element: <EventDetailPage />,
+								action: deleteEventAction,
+							},
+							{
+								path: 'edit',
+								element: <EditEventPage />,
+								action: manipulateEventAction,
+							},
 						],
 					},
-					{ path: 'new', element: <NewEventPage />, action: manipulateEventAction },
+					{
+						path: 'new',
+						element: <NewEventPage />,
+						action: manipulateEventAction,
+					},
 				],
+			},
+			{
+				path: 'newsletter',
+				element: <NewsletterPage />,
+				action: newsletterAction,
 			},
 		],
 	},
