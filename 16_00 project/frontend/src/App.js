4a1a440a832ac0c8e42d963abbbcda51503d1206
Aleksa21000 <radulovicaleksa21@gmail.com>
2024-03-02 20:06:59 +0100

    re-using action with more flexible aproach with methods as props

--- 16_00 project/frontend/src/App.js
+++ 16_00 project/frontend/src/App.js
@@ -6,8 +6,9 @@ import Homepage from './pages/Home';
 import RootEventsPage from './pages/RootEvents';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetail';
-import NewEventPage, { action as newEventAction } from './pages/NewEvent';
+import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([
 	{
@@ -31,10 +32,10 @@ const router = createBrowserRouter([
 						loader: eventDetailLoader,
 						children: [
 							{ index: true, element: <EventDetailPage />, action: deleteEventAction },
-							{ path: 'edit', element: <EditEventPage /> },
+							{ path: 'edit', element: <EditEventPage />, action: manipulateEventAction },
 						],
 					},
-					{ path: 'new', element: <NewEventPage />, action: newEventAction },
+					{ path: 'new', element: <NewEventPage />, action: manipulateEventAction },
 				],
 			},
 		],
