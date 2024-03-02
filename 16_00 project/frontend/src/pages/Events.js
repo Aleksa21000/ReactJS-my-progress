e82bd977fd43ad3a4024a4979d4ecf79ce595434
Aleksa21000 <radulovicaleksa21@gmail.com>
2024-03-02 14:04:26 +0100

    introduced loader & fetching with him, also basic usage

--- 16_00 project/frontend/src/pages/Events.js
+++ 16_00 project/frontend/src/pages/Events.js
@@ -1,23 +1,11 @@
-import { Link } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
-const DUMMY_EVENTS = [
-	{ id: 'e1', title: 'First Element' },
-	{ id: 'e2', title: 'Second Element' },
-];
+import EventsList from '../components/EventsList';
 
-const EventsPage = () => {
-	return (
-		<>
-			<h1>EventsPage</h1>
-			<ul>
-				{DUMMY_EVENTS.map((event) => (
-					<li key={event.id}>
-						<Link to={event.id}>{event.title}</Link>
-					</li>
-				))}
-			</ul>
-		</>
-	);
-};
+function EventsPage() {
+	const events = useLoaderData();
+
+	return <EventsList events={events} />;
+}
 
 export default EventsPage;
